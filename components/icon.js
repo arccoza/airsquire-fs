b69b6e3722907659172dde957e177852e46c9eef
Adrien <adrien@arccoza.com>
2021-05-15 16:22:41 +0800

    Remove old icon comments

--- components/icon.js
+++ components/icon.js
@@ -1,14 +1,6 @@
 import styles from './icon.module.css'
 
 
-// function Icon({ name }) {
-//   return <svg dangerouslySetInnerHTML={{__html: `<use href="#${name}" xlink:href="#${name}"/>`}}></svg>
-// }
-
-// function Icon({ name }) {
-//   return <i className={``} />
-// }
-
 export default function Icon({ name }) {
   return (
     <>
