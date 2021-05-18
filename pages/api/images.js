4c10f991902b5773feb4fad00b3f5d6df7ce3dd1
Adrien <adrien@arccoza.com>
2021-05-19 03:50:01 +0800

    Add backend images put and search API

---
+++ pages/api/images.js
@@ -0,0 +1,38 @@
+import store from '../../lib/store.js'
+
+
+export default async function images(req, res) {
+  if (req.method == 'GET')
+    return getImages(req, res)
+  else if (req.method == 'PUT')
+    return putImages(req, res)
+  return Promise.resolve(res.status(405).json({}))
+}
+
+async function getImages(req, res) {
+  console.log(req.query)
+  var p = new Promise((resolve, reject) => {
+    const stream = store.listObjectsV2('airsquire', `images/${req.query.s}`, true, '')
+    // stream.on('error', function(err) { console.log(err), reject(err) } )
+    stream.on('readable', () => {
+      let item, items = []
+      while (item = stream.read()) {
+        console.log(item)
+        items.push(item)
+      }
+      resolve(items)
+    })
+  })
+  return p.then(ls => res.json(ls)).catch(err => res.json(err))
+}
+
+async function putImages(req, res) {
+  try {
+    req.body
+  } catch (error) {
+    return Promise.resolve(res.status(400).json({ error: 'Bad json data' }))
+  }
+
+  return store.presignedPutObject('airsquire', `/images/${req.body.name}`)
+    .then(url => res.json({url})).catch(err => res.json(err))
+}
