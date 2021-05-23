ab954ceea17ffc0f47597a9bd0c4dd0718378ddc
Adrien <adrien@arccoza.com>
2021-05-24 02:40:00 +0800

    Update Gallery with uploading placeholder

--- components/gallery.js
+++ components/gallery.js
@@ -2,14 +2,19 @@ import styles from './gallery.module.css'
 import Image from 'next/image'
 
 
+const blankImg = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjE2MCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4='
+
 export default function Gallery({ imageData=[] }) {
+  console.log('-->', imageData.map(i => i.name))
+  var loader = {loader: (({ src }) => src)} // workaround for `blob:` urls
 	return (
 		<div className={styles.gallery}>
-      {imageData.map(({ etag, name, url }) => (
-        <div key={etag}>
+      {imageData.map(({ id=null, name, url='' }) => (
+        <div key={name} className={id ? '' : styles.placeholder} >
           <Image
             priority
-            src={url}
+            {...(!id ? loader : {})}
+            src={url || blankImg}
             alt={name}
             width={320}
             height={160}
