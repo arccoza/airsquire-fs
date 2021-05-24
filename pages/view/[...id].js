9f69247cf7cec115895c0d07a427eac62bcade33
Adrien <adrien@arccoza.com>
2021-05-24 15:52:35 +0800

    Add pano viewer page

---
+++ pages/view/[...id].js
@@ -0,0 +1,14 @@
+import Panolens from '../../components/panolens'
+import { useRouter } from 'next/router'
+import { urlPrefix } from '../../lib/store-vars'
+
+
+export default function View() {
+  const router = useRouter()
+  const id = router.query.id
+  const url = urlPrefix + id.join('/')
+
+  return (
+    <Panolens image={url}></Panolens>
+  )
+}
