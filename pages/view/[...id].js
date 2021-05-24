13f4ec796b5c67d5ba8dd4839b9890a1e238a443
Adrien <adrien@arccoza.com>
2021-05-24 23:55:17 +0800

    Fix join on id if id is null

--- pages/view/[...id].js
+++ pages/view/[...id].js
@@ -6,7 +6,7 @@ import { urlPrefix } from '../../lib/store-vars'
 export default function View() {
   const router = useRouter()
   const id = router.query.id
-  const url = urlPrefix + id.join('/')
+  const url = id && (urlPrefix + id.join('/'))
 
   return (
     <Panolens image={url}></Panolens>
