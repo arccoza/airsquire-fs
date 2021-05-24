22ad1620420370c92d963bca24470ab22a78db7f
Adrien <adrien@arccoza.com>
2021-05-24 15:58:07 +0800

    Fix, add empty domain to workaround blob urls

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
   images: {
-    domains: ['f002.backblazeb2.com', 'az-airsquire.s3.amazonaws.com', 's3.us-east-1.amazonaws.com'],
+    domains: ['', 'f002.backblazeb2.com', 'az-airsquire.s3.amazonaws.com', 's3.us-east-1.amazonaws.com'],
   },
 }
