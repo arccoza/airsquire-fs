23269707dc87fae82845bdd152d38a52a2a5c2a7
Adrien <adrien@arccoza.com>
2021-05-26 20:11:18 +0800

    Update uploader, make images selectable

--- pages/uploads.js
+++ pages/uploads.js
@@ -1,11 +1,11 @@
 import Gallery from '../components/gallery'
 
 
-export default function Uploads({ state:{uploads} }) {
+export default function Uploads({ dispatch, state:{uploads} }) {
   uploads = Object.values(uploads)
   return (
     <main className={'content'}>
-      <Gallery imageData={uploads} />
+      <Gallery imageData={uploads} onSelect={url => dispatch({type: 'image/select', payload: url})} />
     </main>
   )
 }
