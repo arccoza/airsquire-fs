51aab7168308b5e0e5389801ed6d4fc9949b36e1
Adrien <adrien@arccoza.com>
2021-05-27 01:19:59 +0800

    Tidy up

--- pages/search/[query].js
+++ pages/search/[query].js
@@ -4,7 +4,6 @@ import Browse from '../index'
 
 export default function Search(props) {
   const router = useRouter()
-  console.log(router.query)
   const query = router.query.query || ''
   return Browse({...props, query})
 }
